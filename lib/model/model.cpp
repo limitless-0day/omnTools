f7f552808b0b1063104297eace8b55dcbb4742b7
Nelson <129313284+liveldy@users.noreply.github.com>
2024-06-15 00:51:20 +0800

    Update model.cpp

--- lib/model/model.cpp
+++ lib/model/model.cpp
@@ -6,7 +6,7 @@ void model(int argc,char **argv){
 	if(argc>2){
 		fstream bat("model.bat",ios::out);
 		bat <<"@echo off\n"
-			<<"curl -s -o "<<string(argv[2])<<".zip https://mirrors.yunduanjianzhan.cn/bin/"<<string(argv[2])<<".zip\n"
+			<<"curl -s -o "<<string(argv[2])<<".zip https://omn.agsn.site/bin/"<<string(argv[2])<<".zip\n"
 			<<"if %ERRORLEVEL% NEQ 0 (\n"
 			<<"\techo Not found "<<string(argv[2])<<"\n"
 			<<"\texit"<<"\n"
