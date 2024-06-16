756b8e62647bc78aa0e576ff7078597513cbe846
Nelson <129313284+liveldy@users.noreply.github.com>
2024-06-16 21:10:31 +0800

    Add files via upload

---
+++ header/lang.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifndef LONG_H
+#define LONG_H
+
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <unordered_map>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+
+void lang(int argc,char **argv);
+
+#endif
---
+++ header/tip.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifndef TIP_H
+#define TIP_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+void tip(int argc,char **argv);
+
+#endif
