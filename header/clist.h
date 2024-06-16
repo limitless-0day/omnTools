778cdbf5512963adacf41a9ff9cf18ecdd0efd0a
Nelson <129313284+liveldy@users.noreply.github.com>
2024-06-16 23:43:22 +0800

    Update and rename tip.h to clist.h

--- header/clist.h
+++ header/clist.h
@@ -1,12 +1,12 @@
 #pragma once
 
-#ifndef TIP_H
-#define TIP_H
+#ifndef CLIST_H
+#define CLIST_H
 
 #include <iostream>
 #include <fstream>
 #include <string>
 
-void tip(int argc,char **argv);
+void clist(int argc,char **argv);
 
 #endif
