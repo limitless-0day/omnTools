2c80d4e14182f3115395fde7f35ec8f97f7c6526
Nelson <129313284+liveldy@users.noreply.github.com>
2024-06-20 22:44:00 +0800

    Add files via upload

---
+++ header/stats.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifndef STATS_H
+#define STATS_H
+
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <unordered_map>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+
+void stats(int argc,char **argv);
+
+#endif
